313caeb3faec0f40c85fa162f5f86486c4573b7b
Matin-Salem <mohammadmatinsalem@gmail.com>
2026-02-14 16:12:06 +0330

    Add files via upload

---
+++ Needle/Needle.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+	int strStr(string haystack, string needle) 
+	{
+		for (int i = 0; i < haystack.size() - needle.size(); i++)
+		{
+			bool found = true;
+			for (int j = 0; j < needle.size(); j++)
+			{
+				if (haystack[i+j] != needle[j])
+				{
+					found = false;
+					break;
+				}
+			}
+			if (found)
+				return i;
+		}
+		return -1;
+	}
+};
+int main()
+{
+	string haystack = "rdsfrr", needle = "rd";
+	Solution slt;
+	int result = slt.strStr(haystack, needle);
+	cout << result;
+}
